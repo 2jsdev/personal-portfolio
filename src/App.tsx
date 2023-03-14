1eae3f6af63c4acc64e5782c01096a07f89ccc45
Jordy Morales <dev.morales.jordy@gmail.com>
2023-03-14 10:43:10 -0400

    adding scrollreveal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { ThemeProvider } from "./context/ThemeContext";
 import { LocaleProvider } from "./context/LocaleContext";
+import ScrollReveal from 'scrollreveal'
 import Header from "./components/Header";
 import Home from "./sections/Home";
 import About from "./sections/About";
@@ -12,11 +13,24 @@ import Contact from "./sections/Contact";
 import Footer from "./components/Footer";
 import Spinner from "./components/Spinner";
 
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+
 const App: React.FC = () => {
   const { i18n } = useTranslation();
   const i18nInitialized = i18n.isInitialized;
 
   if (!i18nInitialized) return <Spinner />;
+
+  useEffect(() => {
+    sr.reveal(`.home__data`);
+    sr.reveal(`.home__handle`, { delay: 700 });
+    sr.reveal(`.home__social, .home__scroll`, { delay: 900, origin: "bottom" });
+  }, []);
 
   return (
     <ThemeProvider>
